e057cb849f89f6af63606e7f0f27d64a281e2988
RohitVerma882 <rohitkumar882333@gmail.com>
2022-11-22 00:35:24 +0530

    update

--- cmake/libbase.cmake
+++ cmake/libbase.cmake
@@ -1,19 +1,21 @@
 add_library(libbase STATIC
-    ${SRC_PATH}/libbase/abi_compatibility.cpp
-    ${SRC_PATH}/libbase/chrono_utils.cpp
-    ${SRC_PATH}/libbase/cmsg.cpp
     ${SRC_PATH}/libbase/errors_unix.cpp
-    ${SRC_PATH}/libbase/file.cpp
-    ${SRC_PATH}/libbase/logging.cpp
-    ${SRC_PATH}/libbase/mapped_file.cpp
-    ${SRC_PATH}/libbase/parsebool.cpp
-    ${SRC_PATH}/libbase/parsenetaddress.cpp
-    ${SRC_PATH}/libbase/process.cpp
-    ${SRC_PATH}/libbase/properties.cpp
-    ${SRC_PATH}/libbase/stringprintf.cpp
-    ${SRC_PATH}/libbase/strings.cpp
-    ${SRC_PATH}/libbase/test_utils.cpp
-    ${SRC_PATH}/libbase/threads.cpp)
+    ${SRC_PATH}/libbase/abi_compatibility.cpp
+	${SRC_PATH}/libbase/chrono_utils.cpp
+	${SRC_PATH}/libbase/cmsg.cpp
+	${SRC_PATH}/libbase/file.cpp
+	${SRC_PATH}/libbase/hex.cpp
+	${SRC_PATH}/libbase/logging.cpp
+	${SRC_PATH}/libbase/mapped_file.cpp
+	${SRC_PATH}/libbase/parsebool.cpp
+	${SRC_PATH}/libbase/parsenetaddress.cpp
+	${SRC_PATH}/libbase/posix_strerror_r.cpp
+	${SRC_PATH}/libbase/process.cpp
+	${SRC_PATH}/libbase/properties.cpp
+	${SRC_PATH}/libbase/stringprintf.cpp
+	${SRC_PATH}/libbase/strings.cpp
+	${SRC_PATH}/libbase/threads.cpp
+	${SRC_PATH}/libbase/test_utils.cpp)
 
 target_include_directories(libbase PUBLIC
     ${SRC_PATH}/libbase/include 
